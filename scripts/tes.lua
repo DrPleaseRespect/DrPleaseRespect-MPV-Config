0aaa211d70b534d413f6e7278554e9a1b5084fe3
DrPleaseRespect <61642760+DrPleaseRespect@users.noreply.github.com>
2021-09-30 15:38:55 +0800

    Update tes.lua

--- scripts/tes.lua
+++ scripts/tes.lua
@@ -1,3 +1,3 @@
 local ass_start = mp.get_property_osd("osd-ass-cc/0")
 local ass_stop = mp.get_property_osd("osd-ass-cc/1")
-mp.osd_message(ass_start .. "{\\an3}{\\fnArcadeClassic}{\\fs10}{\\c&Hffafe5&}MPV Player{\\c&H0000ff&} - {\\c&Haee5ff&}TESFIA Configuration" .. ass_stop,3)
+mp.osd_message(ass_start .. "{\\an3}{\\fnUbuntu}{\\fs10}{\\c&Hffafe5&}MPV Player{\\c&H0000ff&} - {\\c&Haee5ff&}TESFIA Configuration" .. ass_stop,3)
