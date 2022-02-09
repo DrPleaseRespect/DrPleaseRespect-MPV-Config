5731a6b77e96c8932af3b0f61bd50e488447e52b
DrPleaseRespect <61642760+DrPleaseRespect@users.noreply.github.com>
2022-02-09 11:23:37 +0800

    Added Toggle Playlist Looping in UOSC Menu
    Updated Tes.lua ("MPV Player - TESFIA Configuration" Text that shows during start) to now use a osd-overlay instead of using mp.osd_message and also add a fade animation

--- scripts/tes.lua
+++ scripts/tes.lua
@@ -1,3 +1,26 @@
-local ass_start = mp.get_property_osd("osd-ass-cc/0")
-local ass_stop = mp.get_property_osd("osd-ass-cc/1")
-mp.osd_message(ass_start .. "{\\an3}{\\fnUbuntu}{\\fs10}{\\c&Hffafe5&}MPV Player{\\c&H0000ff&} - {\\c&Haee5ff&}TESFIA Configuration" .. ass_stop,3)
+osd = mp.create_osd_overlay("ass-events")
+osd_raw = "{\\an3}{\\fnUbuntu}{\\fs20}{\\c&Hffafe5&}MPV Player{\\c&H0000ff&} - {\\c&Haee5ff&}TESFIA Configuration"
+osd.data = osd_raw
+
+osd:update()
+
+fade = false
+mp.add_periodic_timer(6, function() fade = true end)
+
+alpha = 0
+fade_timer = mp.add_periodic_timer(0.01, function() 
+    if fade == true then
+        alpha = alpha + 5
+        if alpha > 255 then
+            alpha = 255
+            fade = false
+            fade_timer:kill()
+        end
+        local hex = string.format("%X", tostring(alpha)) 
+        local osd_opacity = "{\\alpha&H" .. hex .. "}"
+        osd.data = osd_opacity .. osd_raw
+        osd:update()
+    end
+end)
+
+--mp.osd_message(ass_start .. "{\\an3}{\\fnUbuntu}{\\fs10}{\\c&Hffafe5&}MPV Player{\\c&H0000ff&} - {\\c&Haee5ff&}TESFIA Configuration" .. ass_stop,3)
