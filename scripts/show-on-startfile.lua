a32ec4e21bb6a3c5d5dd52a52de536515aebe2a3
DrPleaseRespect <61642760+DrPleaseRespect@users.noreply.github.com>
2022-07-15 05:16:05 +0800

    Update show-on-startfile.lua

--- scripts/show-on-startfile.lua
+++ scripts/show-on-startfile.lua
@@ -1,6 +1,9 @@
+ontop_finished = false
+ontop_boolean = mp.get_property_bool("ontop")
 
 
 function set_ontop()
+	ontop_finished = false
 	ontop_boolean = mp.get_property_bool("ontop")
 	if ontop_boolean ~= true then
 		mp.set_property_bool("ontop", true)
@@ -8,13 +11,16 @@ function set_ontop()
 end
 
 function stop_ontop()
-	if ontop_boolean ~= true then
-		mp.set_property_bool("ontop", false)
+	if ontop_finished ~= true then
+		if ontop_boolean ~= true then
+			mp.set_property_bool("ontop", false)
+		end
 	end
+	ontop_finished = true
 end
 
 
 
 
-mp.register_event("start-file", set_ontop)
-mp.register_event("file-loaded", stop_ontop)
+mp.register_event("file-loaded", set_ontop)
+mp.register_event("playback-restart", stop_ontop)
