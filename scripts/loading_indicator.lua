f8f86fd2fd4c0617b1c5b5ef741d5ddc51df7965
DrPleaseRespect <juliannayr2007@gmail.com>
2022-10-20 20:12:42 +0800

    Remove Buffering Logic from Loading Indicator
    Not needed since UOSC provides it's own buffering indicator

--- scripts/loading_indicator.lua
+++ scripts/loading_indicator.lua
@@ -28,41 +28,6 @@ function loading()
 	end
 end
 
-function buffering()
-	loading_bool = true
-	local ass_raw = "{\\an5}{\\fnBebasNeue}{\\fs80}{\\c&Hffafe5&}BUFFERING"
-	osd.data = ass_raw
-	osd:update()
-	if debug_flag == true then
-		print("Buffering OSD Loaded")
-	end
-end
-
-
-
-function buffering_state_detection(property, output)
-	if output ~= nil then
-		if output == 'yes' then
-			buffering()
-		else
-			remove_loading()
-		end
-	end
-end
-
-function seek_state_detection()
-	seek_state_timer = mp.add_timeout(0.5, function()
-		local demuxer_state = mp.get_property_native("demuxer-cache-state")
-		if demuxer_state ~= nil then
-			if demuxer_state['underrun'] == true then
-				buffering()
-			else
-				remove_loading()
-			end
-		end
-	end)
-end
-
 function remove_loading(event)
 	if loading_bool == true then
 		loading_bool = false
@@ -75,5 +40,3 @@ end
 
 mp.register_event("start-file",loading)
 mp.register_event("playback-restart", remove_loading)
-mp.observe_property("paused-for-cache", "string", buffering_state_detection)
-mp.register_event("seek", seek_state_detection)
